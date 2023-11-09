d0bcc152b158e9a3e681acc9ccd6fa5fcbc57bc0
toweringcloud <toweringcloud@gmail.com>
2023-11-09 12:53:03 +0900

    [FIX] project homepage subdir

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,9 +3,12 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Coins from "./routes/Coins";
 import Coin from "./routes/Coin";
 
+const homepageTokens = process.env.PUBLIC_URL.split("/");
+const subProjectName = homepageTokens[homepageTokens.length - 1];
+
 function Router() {
 	return (
-		<BrowserRouter>
+		<BrowserRouter basename={subProjectName}>
 			<Switch>
 				<Route path="/:coinId">
 					<Coin />
